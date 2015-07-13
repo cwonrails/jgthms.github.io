71d583e4fda3110332d71893a3073e58a9a2d463
jgthms <bbxdesign@gmail.com>
2015-07-14 00:50:55 +0100

    v10 init

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # A Jekyll blog about the web
 
+jgthms.com
+
 Front-end developer and graphic designer from Bordeaux, France.
---
+++ javascript/jgthms.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function ($) {
+
+    var $toggle = $('.toggle');
+    var $overlay = $('#overlay');
+
+    $toggle.click( function() {
+        var target = $(this).data('target');
+        $('html').addClass(target + '-open');
+    });
+
+    $overlay.click( function() {
+        $('html').removeClass('menu-open elsewhere-open');
+    });
+
+});
