28fd174b492c599cf9d57f945b91fbb90aed6ca5
jgthms <bbxdesign@gmail.com>
2015-10-01 00:37:15 +0100

    proper recommended articles link

--- javascript/articles.js
+++ javascript/articles.js
@@ -1,18 +1,21 @@
 jQuery(document).ready(function ($) {
 
   var $articles = $('#articles');
+  var $title = $articles.find('.heading h1');
   var $switch = $('#switch');
   var recommended = false;
 
   function setRecommended() {
     if (recommended) {
       history.replaceState('', document.title, 'recommended-articles.html');
-      $switch.addClass('is-active');
+      $switch.addClass('is-active').attr('href', 'articles.html');
       $articles.addClass('only-recommended');
+      $title.text('Recommended articles');
     } else {
       history.replaceState('', document.title, 'articles.html');
-      $switch.removeClass('is-active');
+      $switch.removeClass('is-active').attr('href', 'recommended-articles.html');
       $articles.removeClass('only-recommended');
+      $title.text('Articles');
     }
   }
 
@@ -22,7 +25,8 @@ jQuery(document).ready(function ($) {
 
   setRecommended();
 
-  $switch.click(function() {
+  $switch.click(function(e) {
+    e.preventDefault();
     recommended = !recommended;
     setRecommended();
   })
