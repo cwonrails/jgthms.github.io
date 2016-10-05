12aa0404e1555cb2c17bfcb947e7a456e3816750
Jeremy Thomas <bbxdesign@gmail.com>
2016-10-05 23:13:25 +0100

    Add affix

---
+++ javascript/articles.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $('.articles-year, .articles-season').each( function() {
+    $(this).affix({
+      offset: {
+        top: $(this).offset().top - 58
+      }
+    });
+  });
+
+});
--- javascript/projects.js
+++ javascript/projects.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
 
   $('img.lazy').unveil();
 
+  $('.projects-body').each( function() {
+    $(this).affix({
+      offset: {
+        top: $(this).offset().top
+      }
+    });
+  });
+
 });
