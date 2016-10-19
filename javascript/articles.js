965191ed5add658b1dd0a28b0c297941f6b338d5
Jeremy Thomas <bbxdesign@gmail.com>
2016-10-19 23:48:01 +0100

    Better desktop

--- javascript/articles.js
+++ javascript/articles.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.articles-year, .articles-season').each( function() {
     $(this).affix({
       offset: {
-        top: $(this).offset().top - 58
+        top: $(this).offset().top - 68
       }
     });
   });
