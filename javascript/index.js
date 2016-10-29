6d9b2d325746d9ed45b86f0ec397e9395c91e476
Jeremy Thomas <bbxdesign@gmail.com>
2016-10-29 11:06:05 +0100

    Add Arnaud

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  var s = skrollr.init();
+  var timer;
+  var $index = document.getElementById('index');
+  var viewportHeight = $index.offsetHeight;
+  var fullHeight = $index.scrollHeight;
+
+  $index.addEventListener('scroll', function() {
+    if (timer) {
+      window.clearTimeout(timer);
+    }
+
+    timer = window.setTimeout(function() {
+      var scrollPosition = $index.scrollTop;
+      var percentage = (scrollPosition) / (fullHeight - viewportHeight);
+      var actual = Math.round(percentage * -10);
+      $('.index-image-background').css('transform', 'translateY(' + actual + '%)');
+    }, 100);
+  });
 });
