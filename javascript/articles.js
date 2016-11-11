12bb4166f8371f190ab25851a37f9f3adea076f3
Jeremy Thomas <bbxdesign@gmail.com>
2016-11-11 23:47:45 +0000

    Add rem

--- javascript/articles.js
+++ javascript/articles.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.articles-year, .articles-season').each( function() {
     $(this).affix({
       offset: {
-        top: $(this).offset().top - 68
+        top: $(this).offset().top - 60 - 4 // 18px * 0.225rem
       }
     });
   });
